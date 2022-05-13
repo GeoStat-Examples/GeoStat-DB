03ba4a9c199ab3a96bd25cf1316bf29b6e5dddc7
Falk Heße <falk.hesze@googlemail.com>
2022-05-13 09:45:28 +0200

    analysis file does now generate a results file

--- src/aquifer_variogram_analysis_2d.py
+++ src/aquifer_variogram_analysis_2d.py
@@ -11,22 +11,55 @@ import gstools as gs
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+def init_df():
+    
+    df = pd.DataFrame(columns=["dim", "var", "len_scale",
+                               "nugget", "alpha"])
+    return df
+
+
+def fill_df(df, model):
+    
+    # initialize data of lists.
+    data = {"dim":[model.dim],
+            "var":[model.var], 
+            "len_scale":[model.len_scale],
+            "nugget":[model.nugget],
+            "alpha":[model.alpha]}
+
+    # Create DataFrame
+    data_df = pd.DataFrame(data)
+
+    return df.append(data_df, ignore_index=True)
+
+
+def write_df(df):
+    
+    df.to_csv('../results/aquifer_statistics.csv')
+    
+
 def get_data(site_id):
     
     #read in data from a file
     T_data_df = pd.read_csv("../data/aquifer_T.csv")
-    T_field = np.log(T_data_df["T_m2/d"])
     
-    x = T_data_df["East_UTM_km"]
-    y = T_data_df["North_UTM_km"]
+    df = T_data_df[T_data_df['site_id']==site_id]
+    
+    T_field = np.log(df["T_m2/d"])
+    
+    x = df["East_UTM_km"]
+    y = df["North_UTM_km"]
     
     return x, y, T_field
+
 
 def get_empirical_variogram(x, y, field):
     
     # estimate the empirical variogram of the field
     bin_center, gamma = gs.vario_estimate((x, y), field)
     return bin_center, gamma
+
 
 def fit_model_variogram(bin_center, gamma):
     
@@ -35,15 +68,24 @@ def fit_model_variogram(bin_center, gamma):
     fit_model.fit_variogram(bin_center, gamma, nugget=False)
     return fit_model
 
+
 if __name__ == '__main__':
     
-    x, y, field = get_data(site_id = "Dammam_aquifer")
+    df = init_df()
     
-#    bin_center, gamma = get_empirical_variogram(x, y, field)
-#    fit_model = fit_model_variogram(bin_center, gamma)
+    x, y, field = get_data(site_id = "Dammam_aquifer")   
+    bin_center, gamma = get_empirical_variogram(x, y, field)
+    fit_model = fit_model_variogram(bin_center, gamma)
+    df = fill_df(df, fit_model)
+    
+    
+    
+    write_df(df)
+    
     
     # output
-#    ax = fit_model.plot(x_max=max(bin_center))
-#    plt.scatter(bin_center, gamma)
-#    plt.show()
-#    print(fit_model)
+    ax = fit_model.plot(x_max=max(bin_center))
+    plt.scatter(bin_center, gamma)
+    plt.show()
+    print(fit_model)
+    
